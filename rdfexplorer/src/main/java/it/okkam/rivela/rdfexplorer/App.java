dcbd0bfec00d505fc2f3ff59e741dab55b492510
Luigi Selmi <selmi@okkam.it>
2011-10-04 11:49:19 +0200

    prova

---
+++ rdfexplorer/src/main/java/it/okkam/rivela/rdfexplorer/App.java
@@ -0,0 +1,13 @@
+package it.okkam.rivela.rdfexplorer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
